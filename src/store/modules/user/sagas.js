6369a5bf74984e870d0e26aa2be26d728089a5c3
Anderson Ganhara <aganhara@gmail.com>
2019-11-26 13:07:23 -0300

    Atualizando dados de usuário.

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+import api from '~/services/api';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+    const response = yield call(api.put, 'users', profile);
+    toast.success('Perfil atualizado');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seus dados');
+    yield updateProfileFailure();
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
